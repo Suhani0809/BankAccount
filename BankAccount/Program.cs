fa2e7b06158b9c232eb1e096faae98d671ff2035
Suhani0809 <gargsuhani.08@gmail.com>
2021-07-28 10:03:16 +0530

    Update2

--- BankAccount/Program.cs
+++ BankAccount/Program.cs
@@ -45,7 +45,6 @@ namespace BankAccount
                 Console.WriteLine("Choose the actions to be performed");
                 i = Convert.ToChar(Console.ReadLine());
             }
-            Console.ReadLine();
             
         }
 
