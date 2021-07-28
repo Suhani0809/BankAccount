a255c868fda07a5c3f32a6d9531c16035b8fb10e
Suhani0809 <69790033+Suhani0809@users.noreply.github.com>
2021-07-28 10:38:30 +0530

    Update Program.cs

--- BankAccount/Program.cs
+++ BankAccount/Program.cs
@@ -45,10 +45,7 @@ namespace BankAccount
                 Console.WriteLine("Choose the actions to be performed");
                 i = Convert.ToChar(Console.ReadLine());
             }
-            Console.WriteLine("Hello World!!");
             
-            
-            Console.WriteLine("Hello user..!!");
         }
 
     }
